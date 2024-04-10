1515d3990a5b7995f5dd69ee51b7a802e656328e
Nemanja Jevtić <njevtic22@gmail.com>
2024-04-10 22:10:37 +0200

    Changes to file upload

--- shop-api/sparkjava-shop/src/main/java/com/instrument/shop/controller/ImageController.java
+++ shop-api/sparkjava-shop/src/main/java/com/instrument/shop/controller/ImageController.java
@@ -1,14 +1,10 @@
 package com.instrument.shop.controller;
 
-import com.sparkjava.context.annotation.Multipart;
-import com.sparkjava.context.annotation.MultipartText;
-import com.sparkjava.context.annotation.MultipartTextValues;
 import com.sparkjava.context.annotation.MultipartValues;
 import com.sparkjava.context.annotation.PostMapping;
 import com.sparkjava.context.annotation.RequestMapping;
 import jakarta.inject.Singleton;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
@@ -22,57 +18,21 @@ import java.util.Collection;
 @RequestMapping("api/images")
 public class ImageController {
     @PostMapping(consumes = "multipart/form-data")
-    public String uploadImage(
-            @Multipart("image 2") Part image2,
-            @Multipart("image 1") Part image1,
-            @Multipart("pdf") Part pdf,
-
-            @Multipart("emptyString") Part empty,
-            @Multipart(value = "nonExisting", required = false) Part none,
-
-            @Multipart("key") Part key,
-
-            @MultipartValues Collection<Part> allFiles,
-            @MultipartValues(value = {"pdf", "image 1", "key"}, requiredNonEmpty = false) Collection<Part> nonRequiredFiles,
-            @MultipartValues({"key"}) Collection<Part> onlyKeys,
-
-            @MultipartText("strKey") String strKey,
-            @MultipartTextValues Collection<String> allTexts,
-            @MultipartTextValues({"key"}) Collection<String> allKeys,
-            @MultipartTextValues(value = {"empty"}, requiredNonEmpty = false) Collection<String> emptyCollection,
-            @MultipartTextValues(value = {"default1", "default2"}, defaultValues = {"value1", "value2"}) Collection<String> defaultValues,
-            @MultipartTextValues({"strKey", "param"}) Collection<String> texts
-//            @MultipartTextValues({"exception"}) Collection<String> exception
-    ) throws ServletException, IOException {
+    public String uploadImage(@MultipartValues({"images"}) Collection<Part> allFiles) throws IOException {
         File tmpLocal = new File("tmp-local");
         tmpLocal.mkdir();
 
-        validateImageType(image2);
-        InputStream in = image2.getInputStream();
-        Path out = File.createTempFile("tmp-", "-" + image2.getSubmittedFileName(), tmpLocal).toPath();
-        Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING);
-
-        validateImageType(image1);
-        in = image1.getInputStream();
-        out = File.createTempFile("tmp-", "-" + image1.getSubmittedFileName(), tmpLocal).toPath();
-        Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING);
-
-//        validateImageType(pdf);
-        in = pdf.getInputStream();
-        out = File.createTempFile("tmp-", "-" + pdf.getSubmittedFileName(), tmpLocal).toPath();
-        Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING);
-
-        for (Part file : nonRequiredFiles) {
-            in = file.getInputStream();
-            out = File.createTempFile("tmp-", "-" + file.getSubmittedFileName(), tmpLocal).toPath();
+        for (Part file : allFiles) {
+            validateImageType(file.getContentType());
+            InputStream in = file.getInputStream();
+            Path out = File.createTempFile("tmp-", "-" + file.getSubmittedFileName(), tmpLocal).toPath();
             Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING);
         }
 
         return "Uploaded";
     }
 
-    private void validateImageType(Part part) {
-        String imageType = part.getContentType();
+    private void validateImageType(String imageType) {
         if (!imageType.equalsIgnoreCase("image/jpeg")
                 && !imageType.equalsIgnoreCase("image/jpg")
                 && !imageType.equalsIgnoreCase("image/png")) {
