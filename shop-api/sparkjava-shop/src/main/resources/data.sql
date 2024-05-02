3b6c78435626694ee35779f1dfa6b3283df75f7d
Nemanja Jevtić <njevtic22@gmail.com>
2024-05-02 18:54:06 +0200

    Generated database

--- shop-api/sparkjava-shop/src/main/resources/data.sql
+++ shop-api/sparkjava-shop/src/main/resources/data.sql
@@ -1,19 +1,55 @@
-insert into users (id, name, surname, email, username, password, archived, role) values (1, 'Jeremy', 'Jacobson', 'manager1@gmail.com', 'manager1', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (2, 'Jeremy', 'Jacobson', 'manager2@gmail.com', 'manager2', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (3, 'Jeremy', 'Jacobson', 'manager3@gmail.com', 'manager3', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (4, 'Jeremy', 'Jacobson', 'manager4@gmail.com', 'manager4', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (5, 'Jeremy', 'Jacobson', 'manager5@gmail.com', 'manager5', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+-- Passwords are hashed using BCrypt algorithm https://bcrypt-generator.com/
+-- Passwords for all users are:
+--
+-- Script generates database for sparkjava-shop
+-- It generates:
+--	- 3 roles
+--	- 30 users
+--		- 10 managers
+--		- 10 salesmen
+--		- 10 customers
+--
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------- Inserting users --------------------------------------------------------------------------------------------
+insert into users (id, name, surname, email, username, password, archived, role) values (1, 'Rachel', 'Dibbert', 'manager1@gmail.com', 'manager1', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (2, 'Codi', 'VonRueden', 'manager2@gmail.com', 'manager2', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (3, 'Sung', 'Adams', 'manager3@gmail.com', 'manager3', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (4, 'Dwayne', 'Walsh', 'manager4@gmail.com', 'manager4', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (5, 'Laree', 'Bednar', 'manager5@gmail.com', 'manager5', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (6, 'Maura', 'Kunze', 'manager6@gmail.com', 'manager6', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (7, 'Maxine', 'Beer', 'manager7@gmail.com', 'manager7', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (8, 'Sean', 'Lockman', 'manager8@gmail.com', 'manager8', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (9, 'Fleta', 'Reinger', 'manager9@gmail.com', 'manager9', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (10, 'Betty', 'Jenkins', 'manager10@gmail.com', 'manager10', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
 
-insert into users (id, name, surname, email, username, password, archived, role) values (6, 'Jeremy', 'Jacobson', 'salesman1@gmail.com', 'salesman1', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (7, 'Jeremy', 'Jacobson', 'salesman2@gmail.com', 'salesman2', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (8, 'Jeremy', 'Jacobson', 'salesman3@gmail.com', 'salesman3', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (9, 'Jeremy', 'Jacobson', 'salesman4@gmail.com', 'salesman4', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (10, 'Jeremy', 'Jacobson', 'salesman5@gmail.com', 'salesman5', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (11, 'Stefan', 'Gibson', 'salesman1@gmail.com', 'salesman1', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (12, 'Richard', 'Roberts', 'salesman2@gmail.com', 'salesman2', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (13, 'Hulda', 'Harvey', 'salesman3@gmail.com', 'salesman3', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (14, 'Wilhelmina', 'Grady', 'salesman4@gmail.com', 'salesman4', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (15, 'Josef', 'Kemmer', 'salesman5@gmail.com', 'salesman5', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (16, 'Otelia', 'Tremblay', 'salesman6@gmail.com', 'salesman6', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (17, 'Hildred', 'Mitchell', 'salesman7@gmail.com', 'salesman7', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (18, 'Keri', 'Jaskolski', 'salesman8@gmail.com', 'salesman8', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (19, 'Donn', 'Pouros', 'salesman9@gmail.com', 'salesman9', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
+insert into users (id, name, surname, email, username, password, archived, role) values (20, 'Love', 'Luettgen', 'salesman10@gmail.com', 'salesman10', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'SALESMAN');
 
-insert into users (id, name, surname, email, username, password, archived, role) values (11, 'Jeremy', 'Jacobson', 'customer1@gmail.com', 'customer1', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (12, 'Jeremy', 'Jacobson', 'customer2@gmail.com', 'customer2', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (13, 'Jeremy', 'Jacobson', 'customer3@gmail.com', 'customer3', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (14, 'Jeremy', 'Jacobson', 'customer4@gmail.com', 'customer4', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
-insert into users (id, name, surname, email, username, password, archived, role) values (15, 'Jeremy', 'Jacobson', 'customer5@gmail.com', 'customer5', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'MANAGER');
+insert into users (id, name, surname, email, username, password, archived, role) values (21, 'Eugene', 'Frami', 'customer1@gmail.com', 'customer1', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (22, 'Almeta', 'Terry', 'customer2@gmail.com', 'customer2', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (23, 'Bethann', 'VonRueden', 'customer3@gmail.com', 'customer3', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (24, 'Elfrieda', 'Crooks', 'customer4@gmail.com', 'customer4', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (25, 'Chris', 'Senger', 'customer5@gmail.com', 'customer5', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (26, 'Reggie', 'Witting', 'customer6@gmail.com', 'customer6', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (27, 'Elza', 'Lindgren', 'customer7@gmail.com', 'customer7', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (28, 'Silva', 'Kris', 'customer8@gmail.com', 'customer8', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (29, 'Sari', 'Hoeger', 'customer9@gmail.com', 'customer9', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
+insert into users (id, name, surname, email, username, password, archived, role) values (30, 'Latoya', 'Okuneva', 'customer10@gmail.com', 'customer10', '$2a$10$JCYrt8QGHg4suBXWiRgjKu93h5DCq3yFDXMDsTY/Itkgeu3h3pCE6', false, 'CUSTOMER');
 
-alter sequence user_id_seq restart with 16;
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------- Altering user_id_seq -----------------------------------------------------------------------------------------
+alter sequence user_id_seq restart with 31;
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
