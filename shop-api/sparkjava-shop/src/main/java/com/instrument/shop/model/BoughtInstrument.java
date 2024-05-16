58541b8156daf5e781b5c8208a68bd5567f22c37
Nemanja Jevtić <njevtic22@gmail.com>
2024-05-16 16:04:19 +0200

    Updated model

--- shop-api/sparkjava-shop/src/main/java/com/instrument/shop/model/BoughtInstrument.java
+++ shop-api/sparkjava-shop/src/main/java/com/instrument/shop/model/BoughtInstrument.java
@@ -3,6 +3,8 @@ package com.instrument.shop.model;
 import com.instrument.shop.util.Strings;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import java.util.List;
@@ -16,16 +18,21 @@ public class BoughtInstrument extends Instrument {
     @Column(nullable = false)
     private String type;
 
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
     public BoughtInstrument() { }
 
-    public BoughtInstrument(String code, String name, String mark, String description, float price, List<Image> images, int owned, String type) {
-        this(null, code, name, mark, description, price, images, owned, type);
+    public BoughtInstrument(String code, String name, String mark, String description, float price, List<Image> images, int owned, String type, User owner) {
+        this(null, code, name, mark, description, price, images, owned, type, owner);
     }
 
-    public BoughtInstrument(Long id, String code, String name, String mark, String description, float price, List<Image> images, int owned, String type) {
+    public BoughtInstrument(Long id, String code, String name, String mark, String description, float price, List<Image> images, int owned, String type, User owner) {
         super(id, code, name, mark, description, price, images);
         this.owned = owned;
         this.type = Strings.requireNonBlank(type, "Type must not be blank");
+        this.owner = owner;
     }
 
     public int getOwned() {
@@ -34,5 +41,9 @@ public class BoughtInstrument extends Instrument {
 
     public String getType() {
         return type;
+    }
+
+    public User getOwner() {
+        return owner;
     }
 }
