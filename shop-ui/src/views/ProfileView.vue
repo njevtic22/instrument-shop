31d24461872f75e4c849d74737e689ae17817dcd
Nemanja Jevtić <njevtic22@gmail.com>
2024-06-02 17:52:21 +0200

    Added tabs for changing profile

--- shop-ui/src/views/ProfileView.vue
+++ shop-ui/src/views/ProfileView.vue
@@ -48,20 +48,83 @@
                             ></v-text-field>
                         </v-col>
                     </v-row>
-                    <v-btn color="primary">Change data</v-btn>
+                    <v-btn
+                        @click="showChange = !showChange"
+                        :prepend-icon="
+                            showChange ? 'mdi-chevron-up' : 'mdi-chevron-down'
+                        "
+                        color="primary"
+                        >Change data</v-btn
+                    >
                 </v-card-text>
             </v-col>
         </v-row>
+        <v-expand-transition>
+            <div v-show="showChange">
+                <v-divider></v-divider>
+                <div class="d-flex flex-row">
+                    <v-tabs
+                        v-model="tab"
+                        bg-color="primary"
+                        direction="vertical"
+                    >
+                        <v-tab
+                            prepend-icon="mdi-image-edit"
+                            text="Image"
+                            value="Image"
+                        ></v-tab>
+                        <v-tab
+                            prepend-icon="mdi-account-edit"
+                            text="Profile"
+                            value="Profile"
+                        ></v-tab>
+                        <v-tab
+                            prepend-icon="mdi-lock"
+                            text="Password"
+                            value="Password"
+                        ></v-tab>
+                    </v-tabs>
+                    <v-tabs-window v-model="tab">
+                        <v-tabs-window-item value="Image">
+                            <v-card flat>
+                                <v-card-text>
+                                    <h1>Changing image</h1>
+                                </v-card-text>
+                            </v-card>
+                        </v-tabs-window-item>
+
+                        <v-tabs-window-item value="Profile">
+                            <v-card flat>
+                                <v-card-text>
+                                    <h1>Changing profile</h1>
+                                </v-card-text>
+                            </v-card>
+                        </v-tabs-window-item>
+
+                        <v-tabs-window-item value="Password">
+                            <v-card flat>
+                                <v-card-text>
+                                    <h1>Changing password</h1>
+                                </v-card-text>
+                            </v-card>
+                        </v-tabs-window-item>
+                    </v-tabs-window>
+                </div>
+            </div>
+        </v-expand-transition>
     </v-card>
 </template>
 
 <script setup>
-import { inject } from "vue";
+import { inject, ref } from "vue";
 import { profileState, fetchProfile } from "@/store/profile";
 
 const errorSnack = inject("defaultErrorSnackbar");
 const profile = profileState;
 fetchProfile(errorSnack);
+
+const tab = ref(null);
+const showChange = ref(false);
 </script>
 
 <style scoped>
