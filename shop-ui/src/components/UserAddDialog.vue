6312bac12cd8f14ef803b7eafeaab509503eb5bc
Nemanja Jevtić <njevtic22@gmail.com>
2024-06-23 19:46:46 +0200

    Changes to adding salesmen

--- shop-ui/src/components/UserAddDialog.vue
+++ shop-ui/src/components/UserAddDialog.vue
@@ -2,124 +2,12 @@
     <v-dialog v-model="dialog" width="50%">
         <v-card prepend-icon="mdi-account-plus" title="Add salesman">
             <v-card-text>
-                <v-form ref="form">
-                    <v-row>
-                        <v-col>
-                            <v-text-field
-                                v-model="user.name"
-                                :rules="[rules.required]"
-                                label="Name"
-                            ></v-text-field>
-                        </v-col>
-                        <v-col>
-                            <v-text-field
-                                v-model="user.surname"
-                                :rules="[rules.required]"
-                                label="Surname"
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col>
-                            <v-text-field
-                                v-model="user.email"
-                                :rules="[rules.required, rules.email]"
-                                label="Email"
-                            ></v-text-field>
-                        </v-col>
-                        <v-col>
-                            <v-text-field
-                                v-model="user.username"
-                                :rules="[rules.required]"
-                                label="Username"
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col>
-                            <v-text-field
-                                v-model="user.password"
-                                :rules="[rules.required]"
-                                :type="show.password ? 'text' : 'password'"
-                                :append-inner-icon="
-                                    show.password ? 'mdi-eye' : 'mdi-eye-off'
-                                "
-                                @click:append-inner="
-                                    show.password = !show.password
-                                "
-                                :append-icon="
-                                    show.passwordRules
-                                        ? 'mdi-chevron-up'
-                                        : 'mdi-chevron-down'
-                                "
-                                @click:append="
-                                    show.passwordRules = !show.passwordRules
-                                "
-                                :maxlength="50"
-                                :error="
-                                    !isPasswordValid && Boolean(user.password)
-                                "
-                                label="Password"
-                                required
-                                counter
-                            >
-                                <template v-slot:loader>
-                                    <v-progress-linear
-                                        :model-value="progress.value"
-                                        :color="progress.color"
-                                        height="7"
-                                    ></v-progress-linear>
-                                </template>
-                            </v-text-field>
-                        </v-col>
-                        <v-col>
-                            <v-text-field
-                                v-model="user.repeatedPassword"
-                                :rules="[
-                                    rules.required,
-                                    rules.matchWithNewPassword,
-                                ]"
-                                :type="show.password ? 'text' : 'password'"
-                                :append-inner-icon="
-                                    show.password ? 'mdi-eye' : 'mdi-eye-off'
-                                "
-                                @click:append-inner="
-                                    show.password = !show.password
-                                "
-                                label="Repeated password"
-                                required
-                                ref="repeatedPasswordRef"
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row v-if="!isManager()">
-                        <v-col>
-                            <v-text-field
-                                v-model="user.role"
-                                label="Role"
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-
-                    <v-row>
-                        <v-col>
-                            <v-expand-transition>
-                                <div v-show="show.passwordRules">
-                                    <PasswordStrength
-                                        v-model="user.password"
-                                        @progress-changed="updateProgressBar"
-                                        @password-valid-changed="updateValid"
-                                    ></PasswordStrength>
-                                </div>
-                            </v-expand-transition>
-                        </v-col>
-                    </v-row>
-                </v-form>
+                <UserAddForm v-model="user" ref="userForm"></UserAddForm>
             </v-card-text>
 
             <v-card-actions>
                 <v-btn
-                    :disabled="!isFormValid"
+                    :disabled="!userForm || !userForm.isFormValid"
                     @click="addSalesman"
                     color="primary"
                     variant="elevated"
@@ -133,8 +21,7 @@
 </template>
 
 <script setup>
-import { ref, defineModel, computed, watch, inject } from "vue";
-import { isManager } from "@/store/auth";
+import { ref, defineModel, inject } from "vue";
 import { addUser } from "@/store/user";
 
 const snackbar = inject("snackbar");
@@ -143,62 +30,22 @@ const errorSnack = inject("defaultErrorSnackbar");
 const emit = defineEmits(["user-added"]);
 const dialog = defineModel();
 
-const form = ref(null);
-const repeatedPasswordRef = ref(null);
-
-const progress = ref({
-    value: 0,
-    color: "red-darken-1",
-});
+const userForm = ref(null);
 
 const user = ref({
     name: "",
     surname: "",
     email: "",
     username: "",
-    role: "",
+    role: "SALESMAN",
     password: "",
     repeatedPassword: "",
 });
 
-const isPasswordValid = ref(false);
-
-const show = ref({
-    password: false,
-    passwordRules: false,
-});
-
-const rules = {
-    required: (value) => Boolean(value) || "Required",
-    email: (email) =>
-        /.+@.+\..+/.test(email) || "Email must be valid email adress",
-    matchWithNewPassword: (value) =>
-        value === user.value.password ||
-        "Repeated password does not match with password",
-};
-
-watch(
-    () => user.value.password,
-    () => {
-        if (user.value.password) {
-            repeatedPasswordRef.value.validate();
-        }
-    }
-);
-
-function updateValid(newValid) {
-    isPasswordValid.value = newValid;
-}
-
 async function addSalesman() {
-    const { valid } = await form.value.validate();
-
+    const valid = userForm.value.validate();
     if (!valid) {
         return;
-    }
-
-    if (isManager()) {
-        user.value.role = "SALESMAN";
     }
 
     const successCallback = () => {
@@ -206,7 +53,7 @@ async function addSalesman() {
         emit("user-added");
 
         dialog.value = false;
-        reset();
+        userForm.value.reset();
     };
 
     addUser({ ...user.value }, successCallback, errorSnack);
@@ -214,49 +61,8 @@ async function addSalesman() {
 
 function cancel() {
     dialog.value = false;
-    reset();
+    userForm.value.reset();
 }
-
-function reset() {
-    user.value.name = "";
-    user.value.surname = "";
-    user.value.email = "";
-    user.value.username = "";
-    user.value.role = "";
-    user.value.password = "";
-    user.value.repeatedPassword = "";
-
-    form.value.resetValidation();
-}
-
-function updateProgressBar(newProgres) {
-    progress.value.value = newProgres;
-    progress.value.color = getProgressColor(newProgres);
-}
-
-function getProgressColor(progress) {
-    if (progress <= 25) {
-        return "red-darken-1";
-    }
-
-    if (progress <= 50) {
-        return "orange-darken-1";
-    }
-
-    if (progress <= 75) {
-        return "yellow-darken-1";
-    }
-
-    if (progress < 100) {
-        return "light-blue-darken-1";
-    }
-
-    return "green-darken-1";
-}
-
-const isFormValid = computed(() => {
-    return form.value ? form.value.isValid : false;
-});
 </script>
 
 <style scoped></style>
