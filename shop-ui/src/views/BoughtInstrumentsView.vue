a916bb4e1b842a3f16905ca1ee970495e215044e
Nemanja Jevtić <njevtic22@gmail.com>
2024-06-26 00:14:39 +0200

    Added sort multiselect

--- shop-ui/src/views/BoughtInstrumentsView.vue
+++ shop-ui/src/views/BoughtInstrumentsView.vue
@@ -52,8 +52,30 @@
 
         <v-col cols="3" class="fixed-form">
             <v-form>
-                <v-text-field label="Title"></v-text-field>
-                <v-textarea label="Description"></v-textarea>
+                <h2>Sort</h2>
+                <v-select
+                    v-model="sort"
+                    :items="filteredItems"
+                    :item-props="itemProps"
+                    @update:model-value="updateSortItems"
+                    placeholder="Title"
+                    bg-color="white"
+                    multiple
+                    clearable
+                >
+                    <template v-slot:selection="{ item, index }">
+                        <v-chip v-if="index < 1">
+                            <span>{{ item.title }}</span>
+                        </v-chip>
+                        <span
+                            v-if="index === 1"
+                            class="text-grey text-caption align-self-center"
+                        >
+                            (+{{ sort.length - 1 }} others)
+                        </span>
+                    </template>
+                </v-select>
+                {{ sort }}
             </v-form>
         </v-col>
     </v-row>
@@ -62,7 +84,7 @@
 </template>
 
 <script setup>
-import { inject, ref, onUnmounted } from "vue";
+import { inject, ref, onUnmounted, computed } from "vue";
 import { useRouter } from "vue-router";
 import { isCustomer } from "@/store/auth";
 import {
@@ -78,6 +100,136 @@ const errorSnack = inject("defaultErrorSnackbar");
 
 let page = -1;
 let size = 20;
+
+const sort = ref([]);
+
+const sortItems = ref({
+    1: {
+        show: true,
+        index: 0,
+        title: "Name,asc",
+        value: {
+            key: "name",
+            order: "asc",
+            pairedKey: 2,
+        },
+    },
+    2: {
+        show: true,
+        index: 0,
+        title: "Name,desc",
+        value: {
+            key: "name",
+            order: "desc",
+            pairedKey: 1,
+        },
+    },
+    3: {
+        show: true,
+        index: 0,
+        title: "Type,asc",
+        value: {
+            key: "type",
+            order: "asc",
+            pairedKey: 4,
+        },
+    },
+    4: {
+        show: true,
+        index: 0,
+        title: "Type,desc",
+        value: {
+            key: "type",
+            order: "desc",
+            pairedKey: 3,
+        },
+    },
+    5: {
+        show: true,
+        index: 0,
+        title: "Mark,asc",
+        value: {
+            key: "mark",
+            order: "asc",
+            pairedKey: 6,
+        },
+    },
+    6: {
+        show: true,
+        index: 0,
+        title: "Mark,desc",
+        value: {
+            key: "mark",
+            order: "desc",
+            pairedKey: 5,
+        },
+    },
+    7: {
+        show: true,
+        index: 0,
+        title: "Code,asc",
+        value: {
+            key: "code",
+            order: "asc",
+            pairedKey: 8,
+        },
+    },
+    8: {
+        show: true,
+        index: 0,
+        title: "Code,desc",
+        value: {
+            key: "code",
+            order: "desc",
+            pairedKey: 7,
+        },
+    },
+    9: {
+        show: true,
+        index: 0,
+        title: "Purchased,asc",
+        value: {
+            key: "purchased",
+            order: "asc",
+            pairedKey: 10,
+        },
+    },
+    10: {
+        show: true,
+        index: 0,
+        title: "Purchased,desc",
+        value: {
+            key: "purchased",
+            order: "desc",
+            pairedKey: 9,
+        },
+    },
+});
+
+const filteredItems = computed(() => {
+    return Object.values(sortItems.value).filter((item) => item.show);
+});
+
+function itemProps(item) {
+    return {
+        title: item.index === 0 ? item.title : item.title + " " + item.index,
+    };
+}
+
+function updateSortItems(selected) {
+    Object.values(sortItems.value).forEach((item) => {
+        item.show = true;
+        item.index = 0;
+    });
+
+    selected.forEach((selectValue, index) => {
+        const paired = sortItems.value[selectValue.pairedKey];
+        paired.show = false;
+
+        const select = sortItems.value[paired.value.pairedKey];
+        select.index = index + 1;
+    });
+}
 
 const scrollTarget = ref(null);
 
