a3cf67ea001b993088a00b85efcb3911b76e25ec
Nemanja Jevtić <njevtic22@gmail.com>
2024-07-21 16:19:44 +0200

    Changes to instrument page

--- shop-ui/src/views/InstrumentView.vue
+++ shop-ui/src/views/InstrumentView.vue
@@ -1,56 +1,56 @@
 <template>
-    <v-row>
-        <v-col cols="3">
-            <v-card
-                class="d-flex align-center"
-                height="500px"
-                flat
-                :color="
-                    $vuetify.theme.themes.myCustomLightTheme.colors.background
-                "
-            >
-                <div>
-                    <v-card-item>
-                        <v-card-title>{{ instrument.name }}</v-card-title>
-                        <v-card-title
-                            >Price: {{ instrument.price }}</v-card-title
-                        >
-                    </v-card-item>
-                    <v-card-text>
-                        <div><strong>Type:</strong> {{ instrument.type }}</div>
-                        <div><strong>Mark:</strong> {{ instrument.mark }}</div>
-                        <div v-if="isAvailable()">
-                            <strong>Available:</strong>
-                            {{ instrument.quantity }}
-                        </div>
-                        <div v-if="isCustomer() && isBought()">
-                            <strong>Owned:</strong> {{ instrument.owned }}
-                        </div>
-                        <br />
-                        <div v-if="isBought()">
-                            <strong>Purchased:</strong>
-                            {{ formatDateTime(instrument.purchased) }}
-                        </div>
-                    </v-card-text>
-                </div>
-            </v-card>
-        </v-col>
-        <v-col cols="9">
-            <v-carousel height="500px">
-                <v-carousel-item
-                    v-for="image in instrument.images"
-                    :key="image.id"
-                    :src="image.url"
-                >
-                </v-carousel-item>
-            </v-carousel>
-        </v-col>
-    </v-row>
-    <br />
-    <v-card>
-        <v-card-text>
-            {{ instrument.description }}
-        </v-card-text>
+    <v-card elevation="4">
+        <v-row>
+            <v-col cols="3">
+                <v-card class="d-flex align-center" :height="height" flat>
+                    <div>
+                        <v-card-item>
+                            <v-card-title>{{ instrument.name }}</v-card-title>
+                            <v-card-title>
+                                Price: {{ instrument.price }}
+                            </v-card-title>
+                        </v-card-item>
+                        <v-card-text>
+                            <div>
+                                <strong>Type:</strong> {{ instrument.type }}
+                            </div>
+                            <div>
+                                <strong>Mark:</strong> {{ instrument.mark }}
+                            </div>
+                            <div v-if="isAvailable()">
+                                <strong>Available:</strong>
+                                {{ instrument.quantity }}
+                            </div>
+                            <div v-if="isCustomer() && isBought()">
+                                <strong>Owned:</strong> {{ instrument.owned }}
+                            </div>
+                            <br />
+                            <div v-if="isBought()">
+                                <strong>Purchased:</strong>
+                                {{ formatDateTime(instrument.purchased) }}
+                            </div>
+                        </v-card-text>
+                    </div>
+                </v-card>
+            </v-col>
+            <v-col cols="9">
+                <v-carousel :height="height" show-arrows="hover">
+                    <v-carousel-item
+                        v-for="image in instrument.images"
+                        :key="image.id"
+                        :src="image.url"
+                    >
+                    </v-carousel-item>
+                </v-carousel>
+            </v-col>
+        </v-row>
+
+        <br />
+        <v-card flat>
+            <v-card-text>
+                {{ instrument.description }}
+            </v-card-text>
+        </v-card>
     </v-card>
 </template>
 
@@ -64,6 +64,8 @@ import { formatDateTime } from "@/util/date";
 
 const route = useRoute();
 const errorSnack = inject("defaultErrorSnackbar");
+
+const height = ref("450px");
 
 const instrument = ref({
     id: -1,
