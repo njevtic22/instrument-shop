588e9bf205ac229eaa8ea00a48a828acdefdcfe7
Nemanja Jevtić <njevtic22@gmail.com>
2024-07-21 13:39:17 +0200

    Implemented instrument page

--- shop-ui/src/views/InstrumentView.vue
+++ shop-ui/src/views/InstrumentView.vue
@@ -1,14 +1,66 @@
 <template>
-    <h1>Instrument with id: {{ $route.params.id }}</h1>
-    {{ $route.query }}
-    {{ instrument }}
+    <v-row>
+        <v-col cols="3">
+            <v-card
+                class="d-flex align-center"
+                height="500px"
+                flat
+                :color="
+                    $vuetify.theme.themes.myCustomLightTheme.colors.background
+                "
+            >
+                <div>
+                    <v-card-item>
+                        <v-card-title>{{ instrument.name }}</v-card-title>
+                        <v-card-title
+                            >Price: {{ instrument.price }}</v-card-title
+                        >
+                    </v-card-item>
+                    <v-card-text>
+                        <div><strong>Type:</strong> {{ instrument.type }}</div>
+                        <div><strong>Mark:</strong> {{ instrument.mark }}</div>
+                        <div v-if="isAvailable()">
+                            <strong>Available:</strong>
+                            {{ instrument.quantity }}
+                        </div>
+                        <div v-if="isCustomer() && isBought()">
+                            <strong>Owned:</strong> {{ instrument.owned }}
+                        </div>
+                        <br />
+                        <div v-if="isBought()">
+                            <strong>Purchased:</strong>
+                            {{ formatDateTime(instrument.purchased) }}
+                        </div>
+                    </v-card-text>
+                </div>
+            </v-card>
+        </v-col>
+        <v-col cols="9">
+            <v-carousel height="500px">
+                <v-carousel-item
+                    v-for="image in instrument.images"
+                    :key="image.id"
+                    :src="image.url"
+                >
+                </v-carousel-item>
+            </v-carousel>
+        </v-col>
+    </v-row>
+    <br />
+    <v-card>
+        <v-card-text>
+            {{ instrument.description }}
+        </v-card-text>
+    </v-card>
 </template>
 
 <script setup>
-import { fetchAvailableInstrument } from "@/store/availableInstrument";
-import { fetchBoughtInstrument } from "@/store/boughtInstrument";
 import { inject, ref } from "vue";
 import { useRoute } from "vue-router";
+import { isCustomer } from "@/store/auth";
+import { fetchAvailableInstrument } from "@/store/availableInstrument";
+import { fetchBoughtInstrument } from "@/store/boughtInstrument";
+import { formatDateTime } from "@/util/date";
 
 const route = useRoute();
 const errorSnack = inject("defaultErrorSnackbar");
