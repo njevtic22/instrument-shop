3d2c2e9d2e154dca76bb15e810639c97c7f1d631
Nemanja Jevtić <njevtic22@gmail.com>
2024-07-29 18:42:14 +0200

    Updated date format in date input in receipts filter

--- shop-ui/src/components/ReceiptsFilter.vue
+++ shop-ui/src/components/ReceiptsFilter.vue
@@ -72,39 +72,138 @@
                 hide-details
             ></v-text-field>
         </v-col>
+
         <v-col>
             <div class="py-2">
-                <v-date-input
-                    v-model="filter.issuedAtStart"
-                    @keydown="$event.preventDefault()"
-                    @click:clear="filter.issuedAtStart = null"
-                    density="compact"
-                    placeholder="Start issued at"
-                    hide-details
-                    clearable
-                ></v-date-input>
+                <v-menu v-model="menu1" :close-on-content-click="false">
+                    <template v-slot:activator="{ props }">
+                        <v-text-field
+                            v-model="formattedStart"
+                            v-bind="props"
+                            placeholder="Issued at from"
+                            density="compact"
+                            hide-details
+                            readonly
+                            clearable
+                        ></v-text-field>
+                    </template>
+                    <v-date-picker
+                        v-model="picker.start"
+                        :firstDayOfWeek="1"
+                        hide-header
+                    >
+                        <template v-slot:actions>
+                            <v-btn
+                                :disabled="
+                                    picker.start === filter.issuedAtStart
+                                "
+                                @click="picker.start = filter.issuedAtStart"
+                                style="margin-top: -40px"
+                                variant="text"
+                            >
+                                Cancel
+                            </v-btn>
+                            <!-- filter.value.issuedAtEnd -->
+                            <v-btn
+                                :disabled="
+                                    picker.start === filter.issuedAtStart
+                                "
+                                @click="
+                                    filter.issuedAtStart = picker.start;
+                                    menu1 = false;
+                                "
+                                style="margin-top: -40px"
+                                variant="text"
+                            >
+                                Ok
+                            </v-btn>
+                        </template>
+                    </v-date-picker>
+                </v-menu>
             </div>
 
             <div class="py-2">
-                <v-date-input
-                    v-model="filter.issuedAtEnd"
-                    @keydown="$event.preventDefault()"
-                    @click:clear="filter.issuedAtEnd = null"
-                    class="py-2"
-                    density="compact"
-                    placeholder="End issued at"
-                    hide-details
-                    clearable
-                ></v-date-input>
+                <v-menu v-model="menu2" :close-on-content-click="false">
+                    <template v-slot:activator="{ props }">
+                        <v-text-field
+                            v-model="formattedEnd"
+                            v-bind="props"
+                            placeholder="Issued at to"
+                            density="compact"
+                            hide-details
+                            readonly
+                            clearable
+                        ></v-text-field>
+                    </template>
+                    <v-date-picker
+                        v-model="picker.end"
+                        :firstDayOfWeek="1"
+                        hide-header
+                    >
+                        <template v-slot:actions>
+                            <v-btn
+                                :disabled="picker.end === filter.issuedAtEnd"
+                                @click="picker.end = filter.issuedAtEnd"
+                                style="margin-top: -40px"
+                                variant="text"
+                            >
+                                Cancel
+                            </v-btn>
+                            <v-btn
+                                :disabled="picker.end === filter.issuedAtEnd"
+                                @click="
+                                    filter.issuedAtEnd = picker.end;
+                                    menu2 = false;
+                                "
+                                style="margin-top: -40px"
+                                variant="text"
+                            >
+                                Ok
+                            </v-btn>
+                        </template>
+                    </v-date-picker>
+                </v-menu>
             </div>
         </v-col>
     </v-row>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const emit = defineEmits(["filter"]);
+
+const menu1 = ref(false);
+const menu2 = ref(false);
+
+const picker = ref({
+    start: null,
+    end: null,
+});
+
+const formattedStart = computed({
+    get() {
+        return picker.value.start
+            ? new Date(picker.value.start).toLocaleDateString("sr-RS")
+            : "";
+    },
+    set(newValue) {
+        picker.value.start = newValue;
+        filter.value.issuedAtStart = newValue;
+    },
+});
+
+const formattedEnd = computed({
+    get() {
+        return picker.value.end
+            ? new Date(picker.value.end).toLocaleDateString("sr-RS")
+            : "";
+    },
+    set(newValue) {
+        picker.value.end = newValue;
+        filter.value.issuedAtEnd = newValue;
+    },
+});
 
 const filter = ref({
     code: "",
